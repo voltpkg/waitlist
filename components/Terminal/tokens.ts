8a91a624ebee86e455baedb556fd964aea4eea3e
Tejas Ravishankar <xtremedevx@gmail.com>
2021-08-16 14:04:44 +0400

    format files and improve prompt

--- components/Terminal/tokens.ts
+++ components/Terminal/tokens.ts
@@ -1,60 +1,79 @@
 export type Token = {
-  text: string;
-  color: string;
-  noAnimation?: boolean;
+    text: string;
+    color: string;
+    noAnimation?: boolean;
 };
 
 export const tokens: Token[] = [
-  {
-    text: "Resolving Dependencies ",
-    color: "#FF70B5",
-    noAnimation: true,
-  },
-  { text: "[", color: "white", noAnimation: true },
-  {
-    text: "=====================",
-    color: "#CE8FFF",
-    noAnimation: true,
-  },
-  { text: "] [", color: "white", noAnimation: true },
-  { text: "OK", color: "#1AF194", noAnimation: true },
-  { text: "]\n", color: "white", noAnimation: true },
+    {
+        text: "Resolving Dependencies ",
+        color: "#FF70B5",
+        noAnimation: true,
+    },
+    { text: "[", color: "white", noAnimation: true },
+    {
+        text: "=====================",
+        color: "#CE8FFF",
+        noAnimation: true,
+    },
+    { text: "] [", color: "white", noAnimation: true },
+    { text: "OK", color: "#1AF194", noAnimation: true },
+    { text: "]\n", color: "white", noAnimation: true },
 
-  {
-    text: "Resolved 264 Dependencies in 0.0069 seconds\n",
-    color: "#1EF498",
-    noAnimation: true,
-  },
-  { text: "warn: ", color: "#F0F98B", noAnimation: true },
-  { text: "@next/react-refresh-utils", color: "#6CD6FC", noAnimation: true },
-  { text: " has unmet peer dependency ", color: "white", noAnimation: true },
-  { text: "react-refresh\n", color: "#F0F98B", noAnimation: true },
+    {
+        text: "Resolved 264 Dependencies in 0.01 seconds\n",
+        color: "#1EF498",
+        noAnimation: true,
+    },
+    { text: "warn: ", color: "#F0F98B", noAnimation: true },
+    { text: "@next/react-refresh-utils", color: "#6CD6FC", noAnimation: true },
+    { text: " has unmet peer dependency ", color: "white", noAnimation: true },
+    { text: "react-refresh\n", color: "#F0F98B", noAnimation: true },
 
-  { text: "warn: ", color: "#F0F98B", noAnimation: true },
-  { text: "@next/react-refresh-utils", color: "#6CD6FC", noAnimation: true },
-  { text: " has unmet peer dependency ", color: "white", noAnimation: true },
-  { text: "webpack\n", color: "#F0F98B", noAnimation: true },
+    { text: "warn: ", color: "#F0F98B", noAnimation: true },
+    { text: "@next/react-refresh-utils", color: "#6CD6FC", noAnimation: true },
+    { text: " has unmet peer dependency ", color: "white", noAnimation: true },
+    { text: "webpack\n", color: "#F0F98B", noAnimation: true },
 
-  { text: "warn: ", color: "#F0F98B", noAnimation: true },
-  { text: "@next/react-dev-overlay", color: "#6CD6FC", noAnimation: true },
-  { text: " has unmet peer dependency ", color: "white", noAnimation: true },
-  { text: "react\n", color: "#F0F98B", noAnimation: true },
+    { text: "warn: ", color: "#F0F98B", noAnimation: true },
+    { text: "@next/react-dev-overlay", color: "#6CD6FC", noAnimation: true },
+    { text: " has unmet peer dependency ", color: "white", noAnimation: true },
+    { text: "react\n", color: "#F0F98B", noAnimation: true },
 
-  {
-    text: "Installing Packages ",
-    color: "#FF70B5",
-    noAnimation: true,
-  },
-  { text: "[", color: "white", noAnimation: true },
-  {
-    text: "=====================",
-    color: "#CE8FFF",
-  },
-  { text: "] 264/264\n", color: "white", noAnimation: true },
+    {
+        text: "Installing Packages ",
+        color: "#FF70B5",
+        noAnimation: true,
+    },
+    { text: "[", color: "white", noAnimation: true },
+    {
+        text: "=====================",
+        color: "#CE8FFF",
+    },
+    { text: "] 301/301\n", color: "white", noAnimation: true },
 
-  {
-    text: "Finished in 0.23s",
-    color: "#1EF498",
-    noAnimation: true,
-  },
+    {
+        text: "Finished in 0.21s",
+        color: "#1EF498",
+        noAnimation: true,
+    },
+    {
+        text: " (",
+        color: "white",
+    },
+    {
+        text: "Cached",
+        color: "#6CD6FC",
+        noAnimation: true,
+    },
+    {
+        text: ")",
+        color: "white",
+        noAnimation: true,
+    },
+    {
+        text: "\n/dev/my-project:",
+        color: "white",
+        noAnimation: true,
+    },
 ];
